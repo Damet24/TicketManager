85ba545edcfbd113c6cb0839ce8479b12601edf5
Daniel Mercado <daniel.mercado@masiv.com>
2023-11-08 11:03:20 -0500

    init proyect

---
+++ Ticket.Domain/Repositories/ITicketRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticket.Domain.Repositories;
+
+public interface ITicketRepository
+{
+    public Task Save(Entities.Ticket ticket);
+    public Task<Entities.Ticket?> Get(string id);
+}
---
+++ Ticket.WebApi/Controllers/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using Common.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Ticket.Domain.Entities;
+using Ticket.Domain.Enums;
+using Ticket.Infrastructure.Dtos.Request;
+
+namespace Ticket.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TicketController : ControllerBase
+{
+    private readonly ICommandBus _commandBus;
+
+    public TicketController(ICommandBus commandBus)
+    {
+        _commandBus = commandBus;
+    }
+
+    [HttpPost]
+    public IActionResult CreateTicket([FromBody] TicketRequest request)
+    {
+        var command = new CreateTicketCommand
+        {
+            Author = request.Author,
+            Type = request.Type,
+            Message = request.Message
+        };
+        _commandBus.Dispatch(command);
+        return Created("", null);
+    }
+}
